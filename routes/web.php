a662fc52f84342103b8820d5a79e84873442bb77
Your Name <you@example.com>
2025-06-16 14:52:32 +0900

    top page

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
 
 Route::get('/dashboard', function () {
